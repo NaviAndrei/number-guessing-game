6d209d633f118bad14bdb897556b2b398aa55ee0
NaviAndrei <andrei.ivan1208@gmail.com>
2024-06-25 04:59:27 +0300

    main.py modified validation and error handling added.

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ You have 10 tries to guess it.
 """)
 
 a = input("Enter a number between 1 and 100: ")
+while not a.isnumeric():
+    a = input("Invalid input. Please enter a number: ")
+
+while (int(a) < 1) or (int(a) > 100):
+    a = input("Invalid input. Please enter a number between 1 and 100: ")
+
+
 b = random.randint(1, 100)  # generates a random number between 1 and 100
 n = 0
 time.sleep(1)
@@ -19,10 +26,15 @@ while (int(a) != b) and n < 10:
         print("Too high, try again.")
     else:
         print("Too low, try again.")
-    n = n + 1
+    n += 1
     print("You have " + str(10 - n) + " tries left.")
+    if str(10 - n) == "0":
+        print("You have run out of tries. The number was " + str(b) + ".")
+        print("Thanks for playing!")
+        exit(1)
     a = input("Enter a number between 1 and 100: ")
     time.sleep(1)
-
-print("Congratulations! You guessed it correctly.")
-print("Thanks for playing!")
+    if int(a) == b:
+        print("Congratulations! You guessed it correctly.")
+        print("Thanks for playing!")
+        exit(1)

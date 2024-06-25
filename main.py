63a3b52137314d058757f69b3ebcfe5ec31eceed
NaviAndrei <andrei.ivan1208@gmail.com>
2024-06-25 03:54:26 +0300

    main.py added

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# Number Guessing Game
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import time
 
+print("""
+Welcome to the Number Guessing Game!
+I'm thinking of a number between 1 and 100.
+You have 10 tries to guess it.
+""")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = input("Enter a number between 1 and 100: ")
+b = random.randint(1, 100)  # generates a random number between 1 and 100
+n = 0
+time.sleep(1)
 
+while (int(a) != b) and n < 10:
+    if int(a) > b:
+        print("Too high, try again.")
+    else:
+        print("Too low, try again.")
+    n = n + 1
+    print("You have " + str(10 - n) + " tries left.")
+    a = input("Enter a number between 1 and 100: ")
+    time.sleep(1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Congratulations! You guessed it correctly.")
+print("Thanks for playing!")
